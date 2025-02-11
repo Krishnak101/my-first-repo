c59fdf35254693bc79ad2473d2ad0d481e166b09
Krishnak101 <kunatibala101@gmail.com>
2025-02-11 16:04:08 -0500

    added an option to reset followers

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -1,18 +1,23 @@
-let count =0;
+let count = 0;
 
-function displayCount(){
-    document.getElementById("countDisplay").innerHTML=count;
+function displayCount() {
+    document.getElementById("countDisplay").innerHTML = count;
 
 }
-function increaseCount(){
+function increaseCount() {
     count++;
     displayCount();
     checkCountValue();
 }
-function checkCountValue(){
-    if(count ===10){
+function resetCount() {
+    count = 0;
+    document.getElementById("countDisplay").innerHTML = 0;
+}
+
+function checkCountValue() {
+    if (count === 10) {
         alert("Your Instagram post gained 10 followers! Congratulations!");
-    } else if(count ===20){
+    } else if (count === 20) {
         alert("Your Instagram post gained 20 followers! Keep it up!");
     }
 }
